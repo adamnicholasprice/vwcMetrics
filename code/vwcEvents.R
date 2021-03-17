550ea8eff62c8970ede7c74fb8520ef12ab79a06
Adam Price <adnprice@HydroGeol-iMac2014.local>
2021-03-17 12:57:58 -0700

    Update

--- code/vwcEvents.R
+++ code/vwcEvents.R
@@ -17,12 +17,23 @@
 ############################# Packages #############################
 library(tidyverse)
 library(ggplot2)
+library(lubridate)
 ##   
 ##
 ############################# Code ################################
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Step: Load Testing Data
+# Step: Load testing data and clean up a bit
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 dat = read.csv("./data/N1_MungeVer2_15Nov19_21May20.csv")
+
+dat  = dat %>%
+  select(TIMESTAMP,Ptemp_C,Depth,VWC)%>%
+  mutate(TIMESTAMP = lubridate::as_datetime(TIMESTAMP),
+         Depth = as.factor(Depth)) %>%
+  setNames(c("dateTime","Temperature","Depth","VWC"))
+
+# ggplot(dat)+
+#   geom_point(aes(x=dateTime,y=VWC,color=as.factor(Depth)))
+  
